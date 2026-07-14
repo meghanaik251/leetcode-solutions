a2fc4a5ef81f933ef42e1f9e4cf403ebf05ae9df
Meghana Naik <87764457+meghanaik251@users.noreply.github.com>
2026-07-14 17:27:28 +0530

    Time: 0 ms (100%), Space: 55.7 MB (86.26%) - LeetHub

---
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,35 @@
+var maximumCount = function(nums) {
+  let left = 0;
+  let right = nums.length;
+
+  // Find the first element greater than or equal to 0
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < 0) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  const negativeCount = left;
+
+  left = 0;
+  right = nums.length;
+
+  // Find the first element greater than 0
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] <= 0) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  const positiveCount = nums.length - left;
+
+  return Math.max(negativeCount, positiveCount);
+};
